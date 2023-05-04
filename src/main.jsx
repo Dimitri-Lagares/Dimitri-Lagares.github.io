413a5b066ba999bfd5807afb62c7e4b62c8e0654
Dimitri Lagares Gutiérrez <lagares.dimitri@gmail.com>
2023-05-03 21:40:06 -0500

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import { Requests } from "./components/Requests"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+<React.StrictMode>
+  <HashRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="*" element={<App />} />
+      <Route path="requests" element={<Requests />} />
+    </Routes>
+  </HashRouter>
+</React.StrictMode>,
 )
